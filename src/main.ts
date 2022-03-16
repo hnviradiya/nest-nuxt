76df4f9dd53ba5efa52f38a8eeaf44830dc91419
Hardik Viradiya <hnviradiya@gmail.com>
2022-03-16 15:59:17 +0530

    nuxt js client app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
+import { loadNuxt } from 'nuxt3';
+import { bundle } from '@nuxt/webpack-builder-edge';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Check if we need to run Nuxt in development mode
+  const isDev = process.env.NODE_ENV !== 'production'
+
+  // Get a ready to use Nuxt instance
+  const nuxt = await loadNuxt({ rootDir: 'src/client-app/' })
+
+  // Enable live build & reloading on dev
+  if (isDev) {
+    bundle(nuxt)
+  }
+
+  await app.listen(3001);
 }
 bootstrap();
